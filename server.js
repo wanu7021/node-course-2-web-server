20be127c3e03371d99eb5c91d90415f8bb415841
Manuel Iglesias <miglesias@salesforce.com>
2018-02-08 05:24:33 -0800

    Add project page

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Portfolio'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'unable to handle request'
